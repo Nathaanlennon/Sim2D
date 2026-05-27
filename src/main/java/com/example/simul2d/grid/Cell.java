19ea5be2f60a9c764341950678881aa37fb9f6a0
romeo <romeo.atchori@gmail.com>
2026-05-27 11:44:18 +0200

    class cell grid material

---
+++ src/main/java/com/example/simul2d/grid/Cell.java
@@ -0,0 +1,11 @@
+package com.example.simul2d.grid;
+
+public class Cell {
+    private Material material;
+
+
+    public Cell(Material material) {
+        this.material = material;
+    }
+
+}
---
+++ src/main/java/com/example/simul2d/grid/Grid.java
@@ -0,0 +1,9 @@
+package com.example.simul2d.grid;
+
+public class Grid {
+
+        private int rows;
+        private int columns;
+        private boolean isToric; // True = les bords se touchent, False = murs bloqués
+        private Cell[][] matrix;
+}
