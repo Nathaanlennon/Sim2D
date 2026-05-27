f3eed50f9c9115806eb0d45afce74cfc41aec7e0
romeo <romeo.atchori@gmail.com>
2026-05-27 16:50:58 +0200

    ajout de tous les gueter plus ca gere l'aspect toric

--- src/main/java/com/example/simul2d/grid/Grid.java
+++ src/main/java/com/example/simul2d/grid/Grid.java
@@ -6,5 +6,45 @@ public class Grid {
         private int columns;
         private boolean isToric; // True = les bords se touchent, False = murs bloqués
         private Cell[][] matrix;
+
+        public Grid( int rows, int columns, boolean isToric ){
+                this.rows = rows;
+                this.columns= columns;
+                this.isToric = isToric;
+                this.matrix = new Cell[rows][columns];
+        }
+
+        public int getRows() {
+                return rows;
+        }
+
+        public int getColumns() {
+                return columns;
+        }
+
+        public boolean isToric() {
+                return isToric;
+        }
+
+        public Cell getCell(int r, int c) {
+
+                if (isToric) {
+
+                        int toricR = Math.floorMod(r, rows);
+                        int toricC = Math.floorMod(c, columns);
+
+                        return matrix[toricR][toricC];
+                }
+                else {
+                        // Si pas torique, on vérifie  normalement juste qu'on est bien dans la grille
+                        if (r >= 0 && r < rows && c >= 0 && c < columns){
+                                return matrix[r][c];
+                        }
+                }
+
+                return null;
+        }
+
+
+
 }
-// ff
